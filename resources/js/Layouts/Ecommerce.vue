ffe2353a398be4a20e60f6e41ce840600849ecc0
aqordeon <aqordivika@yahoo.co.id>
2023-06-30 16:27:58 +0800

    Footer: First component

--- resources/js/Layouts/Ecommerce.vue
+++ resources/js/Layouts/Ecommerce.vue
@@ -242,71 +242,14 @@
            <slot/>
         </main>
 
-        <footer aria-labelledby="footer-heading" class="bg-gray-900">
-            <h2 id="footer-heading" class="sr-only">Footer</h2>
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div class="py-20 xl:grid xl:grid-cols-3 xl:gap-8">
-                    <div class="grid grid-cols-2 gap-8 xl:col-span-2">
-                        <div class="space-y-12 md:grid md:grid-cols-2 md:gap-8 md:space-y-0">
-                            <div>
-                                <h3 class="text-sm font-medium text-white">Shop</h3>
-                                <ul role="list" class="mt-6 space-y-6">
-                                    <li v-for="item in footerNavigation.shop" :key="item.name" class="text-sm">
-                                        <a :href="item.href" class="text-gray-300 hover:text-white">{{ item.name }}</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h3 class="text-sm font-medium text-white">Company</h3>
-                                <ul role="list" class="mt-6 space-y-6">
-                                    <li v-for="item in footerNavigation.company" :key="item.name" class="text-sm">
-                                        <a :href="item.href" class="text-gray-300 hover:text-white">{{ item.name }}</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="space-y-12 md:grid md:grid-cols-2 md:gap-8 md:space-y-0">
-                            <div>
-                                <h3 class="text-sm font-medium text-white">Account</h3>
-                                <ul role="list" class="mt-6 space-y-6">
-                                    <li v-for="item in footerNavigation.account" :key="item.name" class="text-sm">
-                                        <a :href="item.href" class="text-gray-300 hover:text-white">{{ item.name }}</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h3 class="text-sm font-medium text-white">Connect</h3>
-                                <ul role="list" class="mt-6 space-y-6">
-                                    <li v-for="item in footerNavigation.connect" :key="item.name" class="text-sm">
-                                        <a :href="item.href" class="text-gray-300 hover:text-white">{{ item.name }}</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mt-12 md:mt-16 xl:mt-0">
-                        <h3 class="text-sm font-medium text-white">Sign up for our newsletter</h3>
-                        <p class="mt-6 text-sm text-gray-300">The latest deals and savings, sent to your inbox weekly.</p>
-                        <form class="mt-2 flex sm:max-w-md">
-                            <label for="email-address" class="sr-only">Email address</label>
-                            <input id="email-address" type="text" autocomplete="email" required="" class="w-full min-w-0 appearance-none rounded-md border border-white bg-white py-2 px-4 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-900" />
-                            <div class="ml-4 flex-shrink-0">
-                                <button type="submit" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-900">Sign up</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                <div class="border-t border-gray-800 py-10">
-                    <p class="text-sm text-gray-400">Copyright &copy; 2021 Your Company, Inc.</p>
-                </div>
-            </div>
-        </footer>
+        <Footer />
+        
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import Footer from '@/Components/Footers/Footer.vue'
 import {
     Dialog,
     DialogPanel,
@@ -397,80 +340,6 @@ const navigation = {
     pages: [
         { name: 'Company', href: '#' },
         { name: 'Stores', href: '#' },
-    ],
-}
-const categories = [
-    {
-        name: 'New Arrivals',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg',
-    },
-    {
-        name: 'Productivity',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-02.jpg',
-    },
-    {
-        name: 'Workspace',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-04.jpg',
-    },
-    {
-        name: 'Accessories',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-05.jpg',
-    },
-    { name: 'Sale', href: '#', imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-03.jpg' },
-]
-const collections = [
-    {
-        name: 'Handcrafted Collection',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-collection-01.jpg',
-        imageAlt: 'Brown leather key ring with brass metal loops and rivets on wood table.',
-        description: 'Keep your phone, keys, and wallet together, so you can lose everything at once.',
-    },
-    {
-        name: 'Organized Desk Collection',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-collection-02.jpg',
-        imageAlt: 'Natural leather mouse pad on white desk next to porcelain mug and keyboard.',
-        description: 'The rest of the house will still be a mess, but your desk will look great.',
-    },
-    {
-        name: 'Focus Collection',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-collection-03.jpg',
-        imageAlt: 'Person placing task list card into walnut card holder next to felt carrying case on leather desk pad.',
-        description: 'Be more productive than enterprise project managers with a single piece of paper.',
-    },
-]
-const footerNavigation = {
-    shop: [
-        { name: 'Bags', href: '#' },
-        { name: 'Tees', href: '#' },
-        { name: 'Objects', href: '#' },
-        { name: 'Home Goods', href: '#' },
-        { name: 'Accessories', href: '#' },
-    ],
-    company: [
-        { name: 'Who we are', href: '#' },
-        { name: 'Sustainability', href: '#' },
-        { name: 'Press', href: '#' },
-        { name: 'Careers', href: '#' },
-        { name: 'Terms & Conditions', href: '#' },
-        { name: 'Privacy', href: '#' },
-    ],
-    account: [
-        { name: 'Manage Account', href: '#' },
-        { name: 'Returns & Exchanges', href: '#' },
-        { name: 'Redeem a Gift Card', href: '#' },
-    ],
-    connect: [
-        { name: 'Contact Us', href: '#' },
-        { name: 'Twitter', href: '#' },
-        { name: 'Instagram', href: '#' },
-        { name: 'Pinterest', href: '#' },
     ],
 }
 
