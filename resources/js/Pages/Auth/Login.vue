cf0a6a6510c0f0ffb26b0a46395856f3241c331e
YudhistaA <aryarajasa0@gmail.com>
2023-06-30 17:10:38 +0800

    fix password Component

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -28,16 +28,13 @@ const submit = () => {
     });
 };
 
-const layoutType='eCommerce';
+// const layoutType= { layout: FocusGuestLayout };
 
-//defineOptions({ layout: FocusGuestLayout })
+// defineOptions({ layout: FocusGuestLayout })
 defineOptions({ layout: Ecommerce })
 
 
 </script>
-
-
-
 
 <template>
     <Head title="Login" />
@@ -64,7 +61,7 @@ defineOptions({ layout: Ecommerce })
                         <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                         <div class="mt-2">
                                 <Password id="password" name="password" type="password"
-                                    class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    class="block rounded-md border-0  text-gray-900 " />
                             </div>
                     </div>
 
