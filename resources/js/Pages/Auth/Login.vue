32b5338bc04c2ee594da32c3d90688510e3c405f
YudhistaA <aryarajasa0@gmail.com>
2023-06-30 16:00:17 +0800

    test

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -28,8 +28,6 @@ const submit = () => {
 
 <template layout="Ecommerce">
     <Head title="Login" />
-
-
     <div class="flex  flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
