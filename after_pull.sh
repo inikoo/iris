ce22462f50ed1af7c524768898e96d657643db7b
Raul Perusquia <raul@inikoo.com>
2023-07-05 10:50:59 +0800

    rm shared file

---
+++ after_pull.sh
@@ -0,0 +1,7 @@
+#
+# Author: Raul Perusquia <raul@inikoo.com>
+# Created: Wed, 05 Jul 2023 11:17:10 Malaysia Time, Kuala Lumpur, Malaysia
+# Copyright (c) 2023, Raul A Perusquia Flores
+#
+
+cp aiku/app/Models/Auth/IsWebUser.php  app/Models/Auth/
