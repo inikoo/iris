b67b72bbfacd50c0c914b48993b977f8359b7bc6
Raul Perusquia <raul@inikoo.com>
2023-07-05 10:56:05 +0800

    Script to pull latest aiku and overwrite shared files

--- pull_aiku.sh
+++ pull_aiku.sh
@@ -3,5 +3,5 @@
 # Created: Wed, 05 Jul 2023 11:17:10 Malaysia Time, Kuala Lumpur, Malaysia
 # Copyright (c) 2023, Raul A Perusquia Flores
 #
-
+git submodule update --remote --merge
 cp aiku/app/Models/Auth/IsWebUser.php  app/Models/Auth/
