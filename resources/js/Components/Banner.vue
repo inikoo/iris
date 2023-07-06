23b77a45fbc587c68dad5b568b0aa2a030351818
aqordeon <aqordivika@yahoo.co.id>
2023-07-06 10:49:16 +0800

    PKA-936 Webpage Banner: add button

--- resources/js/Components/Banner.vue
+++ resources/js/Components/Banner.vue
@@ -11,6 +11,9 @@
             :navigation="false" :modules="[Autoplay, Pagination, Navigation]" class="mySwiper">
             <SwiperSlide v-for="imgBanner in data.imgsBanner">
                 <img :src="imgBanner.imageSrc" :alt="imgBanner.imageAlt" srcset="">
+                <Link v-if="imgBanner.link" :href="imgBanner.link.target" class="bg-gray-800/40 text-gray-100 border border-gray-50/50 absolute bottom-6 right-11 rounded px-3 py-1 hover:bg-gray-900/60">
+                    {{imgBanner.link.label}}
+                </Link>
             </SwiperSlide>
         </swiper>
     </div>
@@ -18,7 +21,8 @@
 
 <script setup lang="ts">
 // Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Link, router } from "@inertiajs/vue3"
 import { Autoplay, Pagination, Navigation } from 'swiper/modules'
 
 // Import Swiper styles
@@ -32,6 +36,10 @@ const data = {
         {
             imageSrc: "https://tailwindui.com/img/ecommerce-images/incentives-07-hero.jpg",
             imageAlt: "Front of men's Basic Tee in sienna",
+            link: {
+                label: "Open",
+                target: "#"
+            }
         },
         {
             imageSrc: "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
@@ -40,6 +48,10 @@ const data = {
         {
             imageSrc: "https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg",
             imageAlt: "Lorem ipsum dolor Basic Tee in sienna",
+            link: {
+                label: "Browse Promo",
+                target: "#"
+            }
         },
         {
             imageSrc: "https://tailwindui.com/img/ecommerce-images/product-page-05-product-01.jpg",
