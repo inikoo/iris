dda97fbc7b7502f27275760fe8f1cf2b2e3c2712
aqordeon <aqordivika@yahoo.co.id>
2023-07-06 13:24:21 +0800

    PKA-939 WebPage: Text

---
+++ resources/js/Components/Text.vue
@@ -0,0 +1,14 @@
+<template>
+    <component :is="data.component">
+        {{ data.value }}
+    </component>
+</template>
+
+<script setup lang="ts">
+const data = {
+    component: "p",
+    value: "Enjoy your best clothes with us"
+}
+</script>
+
+<style scoped></style>
--- resources/js/Pages/Webpage.vue
+++ resources/js/Pages/Webpage.vue
@@ -7,9 +7,11 @@
 <script setup>
 import Baskets from '@/Components/Baskets/Baskets.vue'
 import Banner from '@/Components/Banner.vue'
+import Text from '@/Components/Text.vue'
 </script>
 
 <template layout="Ecommerce">
     <Banner />
+    <Text class="text-2xl text-gray-700 font-bold mt-4 capitalize"/>
     <Baskets theme="1"/>
 </template>
